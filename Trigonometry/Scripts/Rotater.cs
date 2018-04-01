18aec5c4ccc4eb731cbb6d8f2c376caf8dc6a040
Daisky <nakamura.daisky@gmail.com>
2018-04-01 16:52:18 +0900

    rotateシーンの運動の基本完了

---
+++ Trigonometry/Scripts/Rotater.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotater : MonoBehaviour {
+
+    Vector3 force;
+    Vector3 acceleration;
+    Vector3 velocity;
+    
+    private float mass;
+
+    float maxSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+        force = Vector3.zero;
+        acceleration = Vector3.zero;
+        velocity = Vector3.zero;
+
+        mass = 2.0f;
+	}
+	
+
+    public void Move()
+    {
+
+        acceleration = force / mass;
+        velocity += acceleration;
+
+        // Time.deltaTimeが時間の最小単位
+        // velocity は1frameで進む距離
+        // 位置は速度 * 時間で出す
+        gameObject.transform.position += ( velocity * Time.deltaTime);
+        // 力を初期化
+        force = Vector3.zero;
+    }
+
+    public void SlowDown(float ratio)
+    {
+        // F =MA
+        ApplyForce(velocity * mass * -1.0f * ratio);
+    }
+
+    public void ApplyForce( Vector3 addForce)
+    {
+        force += addForce;
+    }
+    public void Reverse()
+    {
+        gameObject.transform.position -= (velocity * Time.deltaTime);
+        velocity *= -1;
+    }
+}
---
+++ Trigonometry/Scripts/RotaterController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotaterController : MonoBehaviour {
+
+    public GameObject rotaterPrefab;
+    GameObject rotater;
+    Rotater script;
+
+    float areaSize = 20.0f;
+	// Use this for initialization
+	void Start () {
+        rotater = Instantiate(rotaterPrefab, Vector3.zero, Quaternion.identity);
+        script = rotater.GetComponent<Rotater>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        script.Move();
+        if (Input.GetMouseButtonDown(0))
+        {
+            script.ApplyForce(RandomVec3(30.0f));
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            script.SlowDown(0.5f);
+        }
+
+        // オブジェクトがエリアを超えた時の処理
+        if(rotater.transform.position.magnitude > areaSize)
+        {
+            script.Reverse();
+        }
+	}
+
+    float RandomFloat(float range)
+    {
+        return Random.Range(-range, range);
+    }
+    Vector3 RandomVec3(float range)
+    {
+        return new Vector3(Random.Range(-range, range), Random.Range(-range, range), Random.Range(-range, range));
+    }
+}
